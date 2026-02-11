00ee9974e6ebc1fca49ace420fb7fa67a9d11e97
Yalinev <a1879948@adelaide.edu.au>
2026-02-11 14:55:52 +1030

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// Order Book Simulator
+/*
+1) Add limit order (buy/sell, price, quantity)
+2) Delete order 
+3) Match trades whenever best bid >= best ask 
+4) Maintain best bid/ask and print trades + top of book 
+
+*Define input format: 
+- ADD: A, id, (buy/sell), price, quantity
+- DELETE: D, id 
+- Print trades as they happen 
+*/
+
+#include <bits/stdc++.h>
